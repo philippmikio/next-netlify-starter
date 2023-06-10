4767add2c8df59ab672ea86f432dfc6148ffb79d
Mikio <mikio@Mikios-Mac-mini-2.local>
2023-06-10 20:08:52 +0200

    third build

---
+++ pages/data.js
@@ -0,0 +1,23 @@
+// data.js
+export const data = {
+    nodes: [
+      { id: '1', name: 'Philipp', x: 0, y: 0 },
+      { id: '2', name: 'Sebastian', x: 0, y: 0 },
+      { id: '3', name: 'Helena', x: 0, y: 0 },
+      { id: '4', name: 'Stefan', x: 0, y: 0 },
+      { id: '5', name: 'Sofia', x: 0, y: 0 },
+      { id: '6', name: 'Rio', x: 0, y: 0 },
+
+      // ...
+    ],
+    edges: [
+      { source: '3', target: '6' },
+      { source: '2', target: '6' },
+      { source: '1', target: '6' },
+      { source: '4', target: '6' },
+      { source: '5', target: '6' },
+      { source: '6', target: '6' },
+      // ...
+    ]
+  };
+  
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,24 @@ import Header from '@components/Header';
 import Footer from '@components/Footer';
 import { useEffect } from 'react';
 import * as d3 from 'd3';
+import { data } from './data.js';
+
+let nodes = data.nodes;
+let edges = data.edges;
+
+// Create a map for quick lookup of nodes using ids
+let nodeMap = new Map(nodes.map(node => [node.id, node]));
+
+// Replace source and target ids in edges with actual node objects
+edges = edges.map(edge => {
+  return {
+    ...edge,
+    source: nodeMap.get(edge.source),
+    target: nodeMap.get(edge.target)
+  }
+});
+
+
 
 export default function Home() {
   useEffect(() => {
@@ -12,21 +30,13 @@ export default function Home() {
       .attr('height', 400)
       .style('border', '1px solid black');
 
-    let nodes = [
-      { id: 'node1', name: 'hello', x: 50, y: 100 },
-      { id: 'node2', name: 'bye', x: 150, y: 100 }
-    ];
-
-    let edges = [
-      { source: nodes[0], target: nodes[1] } // Use node references directly
-    ];
-
-    const simulation = d3.forceSimulation(nodes)
-    .force('charge', d3.forceManyBody().strength(-200)) // Repulsion between nodes
-    .force('x', d3.forceX(200).strength(0.1)) // Gradual attraction to the center X
-    .force('y', d3.forceY(200).strength(0.1)) // Gradual attraction to the center Y
-    .force('collide', d3.forceCollide(25)) // Prevent overlap
-    .force('link', d3.forceLink(edges).distance(100)); // Keep a distance between linked nodes
+      
+const simulation = d3.forceSimulation(nodes)
+.force('charge', d3.forceManyBody().strength(-200))
+.force('x', d3.forceX(200).strength(0.1))
+.force('y', d3.forceY(200).strength(0.1))
+.force('collide', d3.forceCollide(55))
+.force('link', d3.forceLink(edges).distance(100)); 
 
     const nodeElements = svg.selectAll('circle')
       .data(nodes)
@@ -68,25 +78,53 @@ export default function Home() {
         .attr('y2', d => d.target.y);
     };
 
+    const width = 400;  // width of SVG container
+    const height = 400; // height of SVG container
+    const radius = 25;  // radius of nodes
+
     const drag = d3.drag()
-      .on('drag', (event, d) => {
-        d.x = Math.max(20, Math.min(380, event.x)); // 20 and 380 considering node's radius
-        d.y = Math.max(20, Math.min(380, event.y)); // 20 and 380 considering node's radius
-        d3.select(event.sourceEvent.target)
+    .on('start', () => {
+      svg.attr('pointer-events', 'none');  // Disable pointer events on the SVG during dragging
+    })
+    .on('drag', (event, d) => {
+      d.x = Math.max(radius, Math.min(width - radius, event.x)); // Keep nodes within the SVG bounds
+      d.y = Math.max(radius, Math.min(height - radius, event.y)); // Keep nodes within the SVG bounds
+      d3.select(event.sourceEvent.target)
         .attr('cx', d.x)
         .attr('cy', d.y);
       render();
+    })
+    .on('end', () => {
+      svg.attr('pointer-events', 'all');  // Re-enable pointer events on the SVG after dragging
     });
+  
 
   nodeElements.call(drag);
 
   simulation.on('tick', () => {
-    simulation.alpha(1).restart();  // Restart the simulation at every tick
+    nodeElements
+      .attr('cx', d => d.x = Math.max(radius, Math.min(width - radius, d.x)))
+      .attr('cy', d => d.y = Math.max(radius, Math.min(height - radius, d.y)));
+  
+      
+    edgeElements
+      .attr('x1', d => d.source.x)
+      .attr('y1', d => d.source.y)
+      .attr('x2', d => d.target.x)
+      .attr('y2', d => d.target.y);
+  
+    textElements
+      .attr('x', d => d.x)
+      .attr('y', d => d.y);
+  
     render();
-  });
 
-  render();
+      // Manually restart the simulation at every tick. This keeps forces from attenuating over time
+  simulation.alpha(1).restart();
+  });
 }, []);
+
+
 
 
 return (
@@ -108,4 +146,3 @@ return (
   </div>
 );
 }
-
